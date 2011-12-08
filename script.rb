7a6656d50c29d8b889f37c51666c8438387aca3e
Zach <zach.evans@logik.com>
2011-12-08 15:01:05 -0500

    added line to function

--- script.rb
+++ script.rb
@@ -10,4 +10,5 @@ puts "zach finally adding some stuff?"
 def cool_function_from_branch
   puts "this is adam's amazing function"
   puts "crap, I forgot something"
+  puts "last line of function"
 end
